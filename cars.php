08d51270c8241d5a9e836e4bca5ac69c1c7c22af
Jonathan Cady <joncady@uw.edu>
2018-10-21 01:45:44 -0700

    cars

---
+++ cars.php
@@ -0,0 +1,14 @@
+<?
+
+header("Content-type: application/json");
+header("Access-Control-Allow-Origin: *");
+
+$json = array();
+
+foreach (glob("*.jpg") as $filename) {
+    array_push($json, "http://students.washington.edu/joncady/dubhacks/cars/" . $filename);
+}   
+
+echo json_encode($json);
+
+?>
