1cecb1450d0818b8744095d891e20f382f0d6073
Kyle Roland <kcroland@uw.edu>
2018-10-20 20:47:14 -0700

    Add selected behavior

---
+++ landing.js
@@ -0,0 +1,77 @@
+
+//
+// -- your description of what this file does here --
+//
+
+(function() {
+  "use strict";
+
+  let feelings = [];
+
+  /**
+   *  Initializes the page when the window is loaded
+   */
+  window.addEventListener("load", initialize);
+
+  function initialize() {
+    $("col").addEventListener("click", select);
+  }
+
+  function selected() {
+    if (this.classList.contains("selected")) {
+    	this.classList.remove("selected");
+    	var index = array.indexOf(this.innerText);
+			if (index > -1) {
+  			feelings.splice(index, 1);
+  		}
+    } else {
+    	if (feelings.push() < 5) {
+    		this.classList.add("selected");
+      	feelings.push(this.innerText);
+    	}
+    }
+  }
+
+  /**
+   *  Make sure to always add a descriptive comment above
+   *  every function detailing what it's purpose is
+   *  @param {variabletype} someVariable This is a description of someVariable, including, perhaps, preconditions.
+   *  @returns A description of what this function is actually returning
+   */
+  function exampleFunction2(someVariable) {
+    /* SOME CODE */
+    return something;
+  }
+
+  /* ------------------------------ Helper Functions  ------------------------------ */
+  // Note: You may use these in your code, but do remember that your code should not have
+  // any functions defined that are unused.
+
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} id - element ID
+   * @returns {object} DOM object associated with id.
+   */
+  function $(id) {
+    return document.getElementById(id);
+  }
+
+  /**
+   * Returns the first element that matches the given CSS selector.
+   * @param {string} query - CSS query selector.
+   * @returns {object} The first DOM object matching the query.
+   */
+  function qs(query) {
+    return document.querySelector(query);
+  }
+
+  /**
+   * Returns the array of elements that match the given CSS selector.
+   * @param {string} query - CSS query selector
+   * @returns {object[]} array of DOM objects matching the query.
+   */
+  function qsa(query) {
+    return document.querySelectorAll(query);
+  }
+
+})();
